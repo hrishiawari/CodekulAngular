0de331767eafb03379fff7fcf9ecf14a9df0ec13
Melayer <admin@SunilMac.local>
2017-07-26 17:01:47 +0530

    starting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SizerDirective } from './Directives/sizer.directive';
 import { MagicalMathDirective } from './Directives/magical-math.directive';
-
+import { HomeComponent } from './home/home.component';
+import { SidebarComponent } from './home/sidebar/sidebar.component';
+import { MainComponent } from './home/main/main.component';
 @NgModule({
   declarations: [
     AppComponent,
         SizerDirective,
         MagicalMathDirective,
-
+        HomeComponent,
+        SidebarComponent,
+        MainComponent
   ],
   imports: [
     BrowserModule
